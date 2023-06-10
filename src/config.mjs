f9b67305dd58679ecf98f174e5f315c01bbee34e
kokitree <94977757+tharumbun@users.noreply.github.com>
2023-06-10 15:38:58 +0700

    Update config.mjs

--- src/config.mjs
+++ src/config.mjs
@@ -32,7 +32,7 @@ const CONFIG = {
     postsPerPage: 4,
 
     post: {
-      permalink: '/%slug%', // Variables: %slug%, %year%, %month%, %day%, %hour%, %minute%, %second%, %category%
+      permalink: 'posts/%slug%', // Variables: %slug%, %year%, %month%, %day%, %hour%, %minute%, %second%, %category%
       noindex: false,
       disabled: false,
     },
